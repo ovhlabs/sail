dfdd3dd9756e5e563a8ee446ad3b5751ee1089bf
Yvonnick Esnault <yvonnick.esnault@ovh.net>
2015-08-21 18:32:36 +0200

    chore : version for IC

--- version/version.go
+++ version/version.go
@@ -8,13 +8,23 @@ import (
 	"stash.ovh.net/sailabove/sailgo/internal"
 )
 
+var versionNewLine bool
+
+func init() {
+	Cmd.Flags().BoolVarP(&versionNewLine, "versionNewLine", "", true, "New line after version number.")
+}
+
 var Cmd = &cobra.Command{
 	Use:     "version",
 	Short:   "Display Version of sailgo : sailgo version",
 	Long:    `sailgo version`,
 	Aliases: []string{"v"},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Version sailgo : %s\n", internal.VERSION)
-		internal.ReadConfig()
+		if versionNewLine {
+			fmt.Printf("Version sailgo : %s\n", internal.VERSION)
+			internal.ReadConfig()
+		} else {
+			fmt.Printf(internal.VERSION)
+		}
 	},
 }
