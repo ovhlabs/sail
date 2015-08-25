88cb9c1bd3707974de99bb1e38ed9f6812005129
Jean-Tiare Le Bigot <jean-tiare.le-bigot@ovh.net>
2015-08-25 19:41:06 +0000

    feat (me): expose password change. SAILABOVE-719

--- me/me.go
+++ me/me.go
@@ -4,6 +4,7 @@ import "github.com/spf13/cobra"
 
 func init() {
 	Cmd.AddCommand(cmdMeShow)
+	Cmd.AddCommand(cmdMeSetPassword)
 	Cmd.AddCommand(cmdMeSetAcl)
 }
 
---
+++ me/setPassword.go
@@ -0,0 +1,62 @@
+package me
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/howeyc/gopass"
+	"github.com/spf13/cobra"
+	"stash.ovh.net/sailabove/sailgo/internal"
+)
+
+var cmdMeSetPassword = &cobra.Command{
+	Use:   "setPassword",
+	Short: "Set account password: sailgo me setPassword [<password>]",
+	Long: `Set account password: sailgo me setAcl <ip> [<ip> ... ]
+	"example : sailgo me setPassword"
+	Note: After running this command, you will need to run "docker login sailabove.io"
+	`,
+	Aliases: []string{"password", "set-password"},
+	Run:     cmdSetPassword,
+}
+
+// users struct holds all parameters sent to /users
+type users struct {
+	Password string `json:"password,omitempty"`
+}
+
+func cmdSetPassword(cmd *cobra.Command, args []string) {
+	var cmdUsersBody users
+
+	switch len(args) {
+	case 1:
+		cmdUsersBody.Password = args[0]
+	case 0:
+		fmt.Print("Password: ")
+		password := gopass.GetPasswd()
+
+		fmt.Print("Confirm password: ")
+		confirm := gopass.GetPasswd()
+
+		if !bytes.Equal(password, confirm) {
+			fmt.Println("Error : Passwords do not match")
+			return
+		}
+
+		if len(password) == 0 {
+			fmt.Println("Error : Password Required")
+			return
+		}
+
+		cmdUsersBody.Password = string(password[:])
+	default:
+		fmt.Println("Invalid usage. sailgo me password [<password>]. Please see sailgo me password --help")
+		return
+	}
+
+	jsonStr, err := json.Marshal(cmdUsersBody)
+	internal.Check(err)
+	fmt.Println(internal.ReqWantJSON("PUT", http.StatusOK, "/users", jsonStr))
+}
