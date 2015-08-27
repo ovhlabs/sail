7b870c1b3d56d58a5482c76896d05d38539024cf
Pierre Roullon <pierre.roullon@ovh.net>
2015-08-27 12:52:48 +0000

    feat (compose): implement compose up

--- compose/compose.go
+++ compose/compose.go
@@ -1,15 +1,12 @@
 package compose
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	Cmd.AddCommand(cmdComposeUp)
-	Cmd.AddCommand(cmdComposeGet)
+	Cmd.AddCommand(cmdComposeUp())
+	Cmd.AddCommand(cmdComposeGet())
 }
 
 // Cmd compose
@@ -18,22 +15,4 @@ var Cmd = &cobra.Command{
 	Short:   "Compose commands: sail compose --help",
 	Long:    `Compose commands: sail compose <command>`,
 	Aliases: []string{"comp"},
-}
-
-var cmdComposeUp = &cobra.Command{
-	Use:   "up",
-	Short: "sail compose up <namespace>",
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO sail compose up
-		fmt.Fprintln(os.Stderr, "sail compose up TO BE IMPLEMENTED")
-	},
-}
-
-var cmdComposeGet = &cobra.Command{
-	Use:   "get",
-	Short: "sail compose get <namespace>",
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO  sail compose get
-		fmt.Fprintln(os.Stderr, "sail compose get TO BE IMPLEMENTED")
-	},
 }
---
+++ compose/up.go
@@ -0,0 +1,65 @@
+package compose
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"stash.ovh.net/sailabove/sail/internal"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	upFile    string
+	upProject string
+)
+
+func cmdComposeUp() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "up",
+		Short: "sail compose up <namespace>",
+		Run:   cmdUp,
+	}
+
+	wd, err := os.Getwd()
+	if err == nil {
+		t := strings.Split(wd, "/")
+		wd = t[len(t)-1]
+	}
+
+	cmd.Flags().StringVarP(&upFile, "file", "", "docker-compose.yml", "Specify an alternate compose file")
+	cmd.Flags().StringVarP(&upProject, "project-name", "p", wd, "Specify an alternate project name (default: directory name)")
+
+	return cmd
+}
+
+func cmdUp(cmd *cobra.Command, args []string) {
+
+	// Check args
+	if len(args) != 1 {
+		internal.Exit("Invalid usage. sail compose up <namespace>. Please see sail compose up -h\n")
+	}
+	ns := args[0]
+
+	// Try to read file
+	payload, err := ioutil.ReadFile(upFile)
+	if err != nil {
+		internal.Exit("Error reading compose file: %s\n", err)
+	}
+
+	// Execute request
+	path := fmt.Sprintf("/applications/%s/fig/up?stream", ns)
+	reader, _, err := internal.Stream("POST", path, payload,
+		internal.SetHeader("Content-Type", "application/x-yaml"))
+	if err != nil {
+		internal.Exit("Error: %s\n", err)
+	}
+
+	// Display api stream
+	err = internal.DisplayStream(reader)
+	if err != nil {
+		internal.Exit("Error: %s\n", err)
+	}
+}
